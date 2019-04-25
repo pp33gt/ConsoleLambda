23edcf93984d2e3f2e5e5e5f3e4b12a8d3ee02b3
pp33gt <mapundi@outlook.com>
2019-04-25 20:41:06 +0200

    add files

---
+++ Lambda/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Lambda
+{
+    public class SomeClass
+    {
+        public SomeClass()
+        {
+        }
+
+        public string GetHello()
+        {
+            return "Hello";
+        }
+
+        public string GetGoodBye()
+        {
+            return "Good Bye";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExpressionTest();
+
+        }
+
+        private static void ExpressionTest()
+        {
+            Console.WriteLine("Hello World!");
+
+            var sayHello = true;
+
+            Expression<Func<SomeClass, string>> hello = (c => c.GetHello());
+
+            Expression<Func<SomeClass, string>> goodbye = (c => c.GetGoodBye());
+
+            var say = (sayHello) ? hello : goodbye;
+            var methodName = (say.Body as MethodCallExpression).Method.Name;
+        }
+    }
+}
